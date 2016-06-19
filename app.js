dc048f6fa317e9cf6a47c81b6455a5d71850eb82
Simon Sun <blackghost0118@gmail.com>
2016-06-20 00:41:48 +0800

    初始化

---
+++ app.js
@@ -0,0 +1,32 @@
+
+/*
+ * 處理 ssl 相關事務
+ */
+const fs = require('fs');
+const https = require('https');
+const sslKey = fs.readFileSync('./ssl/XXX.key', 'utf8');
+const sslCrt = fs.readFileSync('./ssl/real.crt', 'utf8');
+const sslOptions = {
+    key: sslKey,
+    cert: sslCrt
+};
+
+
+/*
+ * server
+ */
+const express = require('express');
+const app = express();
+const lintBot = require('./lineBot');
+
+/*
+ * 處理 router
+ */
+app.get('/', lintBot);
+
+/*
+ * 啟動 server
+ */
+const httpsServer = https.createServer(sslOptions, app);
+httpsServer.listen(3000);
+console.log('Listen https server at port 3000');
